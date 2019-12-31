8117c034572618041b74275663d00f2a2de710c4
hainessss <hainesaxiv@gmail.com>
2019-12-30 20:18:51 -0800

    test

--- api/carpooly/actions/index.js
+++ api/carpooly/actions/index.js
@@ -1,9 +1,9 @@
 module.exports = async (req, res) => {
-  // try {
-  //   console.log(req.body);
-  // } catch (err) {
-  //   console.log(err)
-  // }
+  try {
+    console.log(req.body);
+  } catch (err) {
+    console.log(err)
+  }
 
   res.status(200).send('hi');
 };
